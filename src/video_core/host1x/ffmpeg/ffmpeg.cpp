8de0ecae627db2aca15dbf6edab5f5c7106d6368
SDK-Chan <sdkchan@eden-emu.dev>
2025-07-14 18:52:40 +0200

    Update src/video_core/host1x/ffmpeg/ffmpeg.cpp

--- src/video_core/host1x/ffmpeg/ffmpeg.cpp
+++ src/video_core/host1x/ffmpeg/ffmpeg.cpp
@@ -27,19 +27,17 @@ constexpr AVPixelFormat PreferredGpuFormat = AV_PIX_FMT_NV12;
 constexpr AVPixelFormat PreferredCpuFormat = AV_PIX_FMT_YUV420P;
 constexpr std::array PreferredGpuDecoders = {
 #ifdef _WIN32
+    AV_HWDEVICE_TYPE_CUDA,
     AV_HWDEVICE_TYPE_D3D11VA,
     AV_HWDEVICE_TYPE_DXVA2,
-    AV_HWDEVICE_TYPE_CUDA,
-    AV_HWDEVICE_TYPE_VULKAN,
 #elif defined(__FreeBSD__)
     AV_HWDEVICE_TYPE_VDPAU,
-    AV_HWDEVICE_TYPE_VULKAN,
 #elif defined(__unix__)
+    AV_HWDEVICE_TYPE_CUDA,
     AV_HWDEVICE_TYPE_VAAPI,
     AV_HWDEVICE_TYPE_VDPAU,
-    AV_HWDEVICE_TYPE_CUDA,
-    AV_HWDEVICE_TYPE_VULKAN,
 #endif
+    AV_HWDEVICE_TYPE_VULKAN,
 };
 
 AVPixelFormat GetGpuFormat(AVCodecContext* codec_context, const AVPixelFormat* pix_fmts) {
